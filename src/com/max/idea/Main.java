04b8d90cc4b2c4b3a0cb762406344fc68caa0046
Касьянова Ксения Юрьевна <KKasyanova@ibs.root.ibscorp.ru>
2022-09-27 21:44:02 +0500

    задача 1

---
+++ src/com/max/idea/Main.java
@@ -0,0 +1,16 @@
+package com.max.idea;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("IBS");
+        System.out.print("Hello ");
+        System.out.println("World");
+        System.out.printf("%s %d равна %f","Половина",1,0.5);
+        System.out.println("\nWorlds\b\n");
+
+    }
+}
+
+
+
